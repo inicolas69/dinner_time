14bbf40d112c57b20ad74468edd408047257b576
Nicolas Iniesta <inicolas69@gmail.com>
2025-02-01 12:09:50 +0100

    Add RecipeIngredient relations in linked models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+
   validates :title, :cook_time, :prep_time, :rating, :image_url, presence: true
 
   before_save :set_total_time, if: -> { changes[:cook_time].present? || changes[:prep_time].present? }
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -2,4 +2,6 @@ require "rails_helper"
 
 RSpec.describe Ingredient, type: :model do
   it { should validate_presence_of(:name) }
+  it { should have_many(:recipe_ingredients) }
+  it { should have_many(:recipes).through(:recipe_ingredients) }
 end
