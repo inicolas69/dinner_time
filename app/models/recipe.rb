d900c11f52580251c8ae3b3a1c947bf76e249a81
Nicolas Iniesta <inicolas69@gmail.com>
2025-02-06 21:26:43 +0100

    Add PgSearch to Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+  include PgSearch::Model
+
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   has_many :ingredient_categories, through: :ingredients
@@ -6,6 +8,13 @@ class Recipe < ApplicationRecord
   validates :title, :cook_time, :prep_time, :rating, :image_url, presence: true
 
   before_save :set_total_time, if: -> { changes[:cook_time].present? || changes[:prep_time].present? }
+
+  pg_search_scope(
+    :pg_search,
+    against: :title,
+    associated_against: { ingredients: :name },
+    using: { tsearch: { prefix: true } },
+  )
 
   private
 
