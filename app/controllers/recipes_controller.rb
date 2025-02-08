4da335163aad693b170cf0c302eb2f8e1c4ae18e
Nicolas Iniesta <inicolas69@gmail.com>
2025-02-08 20:20:45 +0100

    Recipes#Index rework

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,15 +3,30 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
     @ingredient_categories = IngredientCategory.all
 
+    @recipes = @recipes.pg_search(params[:query]).reorder(nil) if params[:query].present?
+
     if params[:ingredient_categories].present?
-      category_ids = search_params[:ingredient_categories]
+      category_ids = params[:ingredient_categories].compact
 
       @recipes = @recipes.joins(ingredients: :ingredient_category)
         .where(ingredient_categories: { id: category_ids })
         .group("recipes.id")
         .having("COUNT(DISTINCT ingredient_categories.id) = ?", category_ids.count)
-    elsif params[:query].present?
-      @recipes = @recipes.pg_search(params[:query])
+    end
+
+    @recipes = @recipes.where("total_time <= ?", params[:total_time]) if params[:total_time].present?
+
+    if params[:sort].present?
+      case params[:sort]
+      when "newest"
+        @recipes = @recipes.order(created_at: :desc)
+      when "oldest"
+        @recipes = @recipes.order(created_at: :asc)
+      when "rating"
+        @recipes = @recipes.order(rating: :desc)
+      when "total_time"
+        @recipes = @recipes.order(total_time: :asc)
+      end
     end
 
     @recipes = @recipes.page(params[:page]).per(12)
@@ -20,11 +35,5 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     @ingredients = @recipe.recipe_ingredients
-  end
-
-  private
-
-  def search_params
-    params.permit(:search, ingredient_categories: [])
   end
 end
