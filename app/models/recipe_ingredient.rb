5b7764c09d946b3d485522005073ee0d3eddcbf8
Nicolas Iniesta <inicolas69@gmail.com>
2025-02-08 18:25:03 +0100

    New methods in RecipeIngredient

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -2,4 +2,10 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
   validates :quantity, presence: true
+
+  delegate :name, to: :ingredient
+
+  def full_ingredient
+    "#{quantity} #{unit.pluralize(quantity)} #{ingredient.name}"
+  end
 end
--- spec/models/recipe_ingredient_spec.rb
+++ spec/models/recipe_ingredient_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe RecipeIngredient, type: :model do
   it { should belong_to(:recipe) }
   it { should belong_to(:ingredient) }
   it { should validate_presence_of(:quantity) }
+  it { should delegate_method(:name).to(:ingredient) }
+
+  describe "#full_ingredient" do
+    it "returns the full ingredient" do
+      recipe_ingredient = create(:recipe_ingredient, quantity: 2, unit: "cup", ingredient: create(:ingredient, name: "flour"))
+
+      expect(recipe_ingredient.full_ingredient).to eq("2 cups flour")
+    end
+  end
 end
